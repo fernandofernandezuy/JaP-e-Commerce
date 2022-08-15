7816468a2d3bdebf63ffe47c30451e0d0a7c22f0
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-15 11:04:33 -0300

    Entrega 1 - Listado de productos

---
+++ js/products.js
@@ -0,0 +1,37 @@
+let currentCategoriesArray = [];
+
+function showCategoriesList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentCategoriesArray.length; i++){
+        let category = currentCategoriesArray[i];
+
+            htmlContentToAppend += `
+            <div onclick="" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${category.image}" alt="${category.name}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${category.name} - ${category.currency} ${category.cost} </h4>
+                            <small class="text-muted">${category.soldCount} artículos</small>
+                        </div>
+                        <p class="mb-1">${category.description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentCategoriesArray = resultObj.data.products
+            showCategoriesList()
+        }
+    })
+});
