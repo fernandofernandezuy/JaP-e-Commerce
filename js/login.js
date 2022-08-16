42ff6ae14f3925d2faa7d86807c9c5d716748cfa
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-16 00:53:19 -0300

    Entrega 1 - Login

---
+++ js/login.js
@@ -0,0 +1,43 @@
+let password = document.getElementById("passwordLgn")
+let email = document.getElementById("emailLgn")
+let emailError = document.getElementById("email-error") 
+let passwordError = document.getElementById("password-error")
+
+function inputErrorAlert(input, inputError) {
+    input.style.border = "1px solid red";
+    input.style.border;
+    input.classList.add("inputlgn")
+    inputError.classList.add("show");
+}
+
+
+function validateLogin() {
+    if (email.value.length === 0 && password.value.length > 0) {
+        inputErrorAlert(email, emailError);
+        email.focus();
+    } else if (email.value.length > 0 && password.value.length === 0) {
+        inputErrorAlert(password, passwordError);
+        password.focus();
+    } else if (email.value.length === 0 && password.value.length === 0) {
+        inputErrorAlert(email, emailError);
+        email.focus();
+        inputErrorAlert(password, passwordError);
+    } else {
+        return true
+    }
+}
+
+function successLogin() {
+
+    if (validateLogin()) {
+        window.location.href = "./inicio.html"
+    }
+}
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+
+    validateLogin();
+    successLogin();
+    
+    });
+
