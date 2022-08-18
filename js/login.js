8eed778d2a72b6f856f2e47241a11fe682088ec2
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-18 18:06:17 -0300

    Pruebas - GoogleAu

--- js/login.js
+++ js/login.js
@@ -10,6 +10,16 @@ function inputErrorAlert(input, inputError) {
     inputError.classList.add("show");
 }
 
+function handleCredentialResponse(response) {
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+ }
 
 function validateLogin() {
     if (email.value.length === 0 && password.value.length > 0) {
