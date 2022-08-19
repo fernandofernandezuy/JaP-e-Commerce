ddf3be78a48160aa31e0f29a1e1a9b2bddbf841e
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-19 11:14:07 -0300

    Changes

--- js/login.js
+++ js/login.js
@@ -3,22 +3,6 @@ let email = document.getElementById("emailLgn")
 let emailError = document.getElementById("email-error")
 let passwordError = document.getElementById("password-error")
 
- function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-  }
-
-  window.onload = function () {
-    google.accounts.id.initialize({
-      client_id: "455520071179-sm5gig5a1asi0mqspvtv9m7bj3p3tg3v.apps.googleusercontent.com",
-      callback: "handleCredentialResponse"
-    });
-    google.accounts.id.renderButton(
-      document.getElementById("g_id_signin"),
-      { theme: "outline", size: "large" } 
-    );
-    google.accounts.id.prompt(); 
-  }
-
   function inputErrorAlert(input, inputError) {
     input.style.border = "1px solid red";
     input.classList.add("inputlgn");
