58a86be1529635726c9e27368f7df3120345852d
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-19 11:46:58 -0300

    Pruebas

--- js/login.js
+++ js/login.js
@@ -3,6 +3,19 @@ let email = document.getElementById("emailLgn")
 let emailError = document.getElementById("email-error")
 let passwordError = document.getElementById("password-error")
 
+function handleCredentialResponse(response) {
+    // decodeJwtResponse() is a custom function defined by you
+    // to decode the credential response.
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+ }
+ 
   function inputErrorAlert(input, inputError) {
     input.style.border = "1px solid red";
     input.classList.add("inputlgn");
