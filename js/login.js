b95efc54eaf4e028fb08bb214e03365128972852
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-19 02:38:01 -0300

    Revert "Pruebas GoogleAu"
    This reverts commit bdddbc822663c638e9399e48dfe92d9b606795aa.

--- js/login.js
+++ js/login.js
@@ -10,17 +10,19 @@ function inputErrorAlert(input, inputError) {
     inputError.classList.add("show");
 }
 
-function onSignIn(googleUser) {
-    let profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); 
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    let id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+ function handleCredentialResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+  window.onload = function () {
+    google.accounts.id.initialize({
+      client_id: "455520071179-sm5gig5a1asi0mqspvtv9m7bj3p3tg3v.apps.googleusercontent.com",
+      callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("g_id_signin"),
+      { theme: "outline", size: "large" } 
+    );
+    google.accounts.id.prompt(); 
   }
 
 function validateLogin() {
