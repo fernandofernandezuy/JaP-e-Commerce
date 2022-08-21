ec17722e7345ba8a95d11f51948919254dd365d5
Fernando Fernández <83631907+fernandofernandezuy@users.noreply.github.com>
2022-08-20 21:36:52 -0300

    Indentacion

--- js/login.js
+++ js/login.js
@@ -1,43 +1,38 @@
-let password = document.getElementById("passwordLgn")
-let email = document.getElementById("emailLgn")
-let emailError = document.getElementById("email-error")
-let passwordError = document.getElementById("password-error")
+let password = document.getElementById("passwordLgn");
+let email = document.getElementById("emailLgn");
+let emailError = document.getElementById("email-error");
+let passwordError = document.getElementById("password-error");
 
 function inputErrorAlert(input, inputError) {
-    input.style.border = "1px solid red";
-    input.classList.add("inputlgn");
-    inputError.classList.add("show");
+  input.style.border = "1px solid red";
+  input.classList.add("inputlgn");
+  inputError.classList.add("show");
 }
 
 function validateLogin() {
-    if (email.value.length === 0 && password.value.length > 0) {
-        inputErrorAlert(email, emailError);
-        email.focus();
-    } else if (email.value.length > 0 && password.value.length === 0) {
-        inputErrorAlert(password, passwordError);
-        password.focus();
-    } else if (email.value.length === 0 && password.value.length === 0) {
-        inputErrorAlert(email, emailError);
-        email.focus();
-        inputErrorAlert(password, passwordError);
-    } else {
-        return true;
-    }
+  if (email.value.length === 0 && password.value.length > 0) {
+    inputErrorAlert(email, emailError);
+    email.focus();
+  } else if (email.value.length > 0 && password.value.length === 0) {
+    inputErrorAlert(password, passwordError);
+    password.focus();
+  } else if (email.value.length === 0 && password.value.length === 0) {
+    inputErrorAlert(email, emailError);
+    email.focus();
+    inputErrorAlert(password, passwordError);
+  } else {
+    return true;
+  }
 }
 
 function redirectLogin() {
-    window.location.href = "./inicio.html"
+  window.location.href = "./inicio.html";
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-
-    document.getElementById("loginBtn").addEventListener("click", function () {
-        if (validateLogin()) {
-            redirectLogin();
-        }
-    });    
-
-    });
-
-
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loginBtn").addEventListener("click", function () {
+    if (validateLogin()) {
+      redirectLogin();
+    }
+  });
+});
